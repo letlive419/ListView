87ae52f3348ffc5a88d83bd2c7370610998dfeab
letlive419 <44731485+letlive419@users.noreply.github.com>
2021-09-13 19:11:23 -0400

    Add files via upload

---
+++ sample-4---listview-ecruz107/Museum Inventory Management/Museum Inventory Management/frmListView.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmListView
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuOpen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.ofdOpen = New System.Windows.Forms.OpenFileDialog()
+        Me.sfdSave = New System.Windows.Forms.SaveFileDialog()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.mnuSave = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuStats = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lvwInventory = New System.Windows.Forms.ListView()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.ViewToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuOpen, Me.ToolStripMenuItem1, Me.mnuSave, Me.ToolStripMenuItem2, Me.mnuExit})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(37, 20)
+        Me.mnuFile.Text = "File"
+        '
+        'mnuOpen
+        '
+        Me.mnuOpen.Name = "mnuOpen"
+        Me.mnuOpen.Size = New System.Drawing.Size(180, 22)
+        Me.mnuOpen.Text = "Open"
+        '
+        'ViewToolStripMenuItem
+        '
+        Me.ViewToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuStats})
+        Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.ViewToolStripMenuItem.Text = "View"
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(187, 400)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(75, 23)
+        Me.btnLoad.TabIndex = 1
+        Me.btnLoad.Text = "Load Data"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(534, 400)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(425, 400)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(309, 400)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 4
+        Me.btnSave.Text = "Save Data"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'ofdOpen
+        '
+        Me.ofdOpen.FileName = "OpenFileDialog1"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(177, 6)
+        '
+        'mnuSave
+        '
+        Me.mnuSave.Name = "mnuSave"
+        Me.mnuSave.Size = New System.Drawing.Size(180, 22)
+        Me.mnuSave.Text = "Save"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(177, 6)
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(180, 22)
+        Me.mnuExit.Text = "Exit"
+        '
+        'mnuStats
+        '
+        Me.mnuStats.Name = "mnuStats"
+        Me.mnuStats.Size = New System.Drawing.Size(180, 22)
+        Me.mnuStats.Text = "Statistics"
+        '
+        'lvwInventory
+        '
+        Me.lvwInventory.HideSelection = False
+        Me.lvwInventory.Location = New System.Drawing.Point(45, 37)
+        Me.lvwInventory.Name = "lvwInventory"
+        Me.lvwInventory.Size = New System.Drawing.Size(716, 357)
+        Me.lvwInventory.TabIndex = 5
+        Me.lvwInventory.UseCompatibleStateImageBehavior = False
+        Me.lvwInventory.View = System.Windows.Forms.View.Details
+        '
+        'frmListView
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lvwInventory)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmListView"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents ofdOpen As OpenFileDialog
+    Friend WithEvents sfdSave As SaveFileDialog
+    Friend WithEvents mnuOpen As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents mnuSave As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripSeparator
+    Friend WithEvents mnuExit As ToolStripMenuItem
+    Friend WithEvents mnuStats As ToolStripMenuItem
+    Friend WithEvents lvwInventory As ListView
+End Class
---
+++ sample-4---listview-ecruz107/Museum Inventory Management/Museum Inventory Management/frmListView.vb
@@ -0,0 +1,217 @@
+﻿Imports System.IO
+Public Class frmListView
+
+    Private strFileName As String
+    Private dblTotalInvValue As Double
+    Private intTotalInvCount As Integer
+    Private arrCategories As ArrayList
+    Private stats As frmStats
+
+    Private Const ARTID As Integer = 0
+    Private Const ARTIST As Integer = 1
+    Private Const TITLE As Integer = 2
+    Private Const DATE_ACQUIRED As Integer = 3
+    Private Const CATEGORY As Integer = 4
+    Private Const CONDITION As Integer = 5
+    Private Const ITEM_LOCATION As Integer = 6
+    Private Const ITEM_VALUE As Integer = 7
+
+    Private Sub openFile()
+        Dim intResult As Integer
+        ofdOpen.InitialDirectory = Application.StartupPath
+        ofdOpen.Filter = "All files (*.*)|*.*| Text files (*.txt)| *.txt"
+        ofdOpen.FilterIndex = 2
+        intResult = ofdOpen.ShowDialog
+        If intResult = DialogResult.Cancel Then
+            Exit Sub
+
+        End If
+        strFileName = ofdOpen.FileName
+        Try
+            ReadInputFile(strFileName)
+        Catch exNotFound As FileNotFoundException
+            MessageBox.Show(exNotFound.ToString)
+
+        Catch exIOError As IOException
+            MessageBox.Show(exIOError.ToString)
+
+        Catch exOther As Exception
+            MessageBox.Show(exOther.ToString)
+        End Try
+    End Sub
+    Private Sub ReadInputFile(strIn As String)
+        Dim fileIn As StreamReader
+        Dim strLineIn As String
+        Dim strFields() As String
+        Dim i As Integer
+        lvwInventory.Items.Clear()
+        Try
+            fileIn = New StreamReader(strIn)
+            If Not fileIn.EndOfStream Then
+                strLineIn = fileIn.ReadLine
+                strFields = strLineIn.Split(",")
+                For i = 0 To strFields.Length - 1
+                    lvwInventory.columns.add(strFields(i))
+                Next
+
+                With lvwInventory
+                    .columns(ARTID).width = 80
+                    .columns(ARTIST).width = 80
+                    .columns(TITLE).width = 150
+                    .columns(DATE_ACQUIRED).width = 80
+                    .columns(CATEGORY).width = 80
+                    .columns(CONDITION).width = 80
+                    .columns(ITEM_LOCATION).width = 80
+                    .columns(ITEM_VALUE).width = 100
+                    .columns(ITEM_VALUE).textAlign = HorizontalAlignment.Right
+                End With
+            End If
+
+
+            While Not fileIn.EndOfStream
+                strLineIn = fileIn.ReadLine
+                strFields = strLineIn.Split(",")
+                Dim lviRow As New ListViewItem(strFields(0))
+                For i = 1 To strFields.Length - 1
+                    Dim lsiCol As New ListViewItem.ListViewSubItem
+                    If i <> ITEM_VALUE Then
+                        lsiCol.Text = strFields(i)
+                    Else
+                        lsiCol.Text = FormatCurrency(strFields(i), 0)
+                    End If
+                    lviRow.SubItems.add(lsiCol)
+                Next
+                lvwInventory.Items.Add(lviRow)
+                UpdateStatistics(lviRow)
+            End While
+            fileIn.Close()
+            fileIn.Dispose()
+        Catch ex As Exception
+            MessageBox.Show("ReadInputFile: " & ex.ToString)
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub WriteOutputFile(strName As String)
+        Dim fileOut As StreamWriter
+        Dim strLineOut As String = ""
+        Dim i As Integer
+        Dim j As Integer
+        Try
+            fileOut = New StreamWriter(strName)
+            For i = 0 To lvwInventory.Columns.Count - 1
+                If i <> lvwInventory.Columns.Count - 1 Then
+                    strLineOut &= lvwInventory.Columns(i).Text & ","
+                Else
+                    strLineOut &= lvwInventory.Columns(i).Text
+                End If
+
+
+            Next
+            fileOut.WriteLine(strLineOut)
+
+            For i = 0 To lvwInventory.Items.Count - 1
+                strLineOut = lvwInventory.Items(i).Text
+                For j = 1 To lvwInventory.Items(i).SubItems.Count - 1
+                    strLineOut &= "," & lvwInventory.Items(i).SubItems(j).Text
+                Next
+                fileOut.WriteLine(strLineOut)
+            Next
+            fileOut.Close()
+            fileOut.Dispose()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+    Private Sub SaveFile()
+        Dim intResult As Integer
+        sfdSave.InitialDirectory = Application.StartupPath
+        sfdSave.Filter = "All files (*.*)|*.*| Text files (*.txt)| *.txt"
+        ofdOpen.FilterIndex = 2
+        intResult = sfdSave.ShowDialog
+        If intResult = DialogResult.Cancel Then
+
+        End If
+        strFileName = sfdSave.FileName
+        Try
+            WriteOutputFile(strFileName)
+        Catch exNotFound As FileNotFoundException
+
+        Catch exIOError As IOException
+
+        Catch exOther As Exception
+
+
+        End Try
+
+    End Sub
+    Private Sub UpdateStatistics(aRow As ListViewItem)
+        Dim blnFoundIt As Boolean
+        For Each aCat As CCategory In arrCategories
+            If aCat.CatName = aRow.SubItems(CATEGORY).Text Then
+                aCat.TotalValue += CDbl(aRow.SubItems(ITEM_VALUE).Text)
+                aCat.TotalCount += 1
+                blnFoundIt = True
+                Exit For
+            End If
+        Next
+        If Not blnFoundIt Then
+            Dim newCat As New CCategory(aRow.SubItems(CATEGORY).Text, CDbl(aRow.SubItems(ITEM_VALUE).Text))
+            arrCategories.Add(newCat)
+
+        End If
+        dblTotalInvValue += CDbl(aRow.SubItems(ITEM_VALUE).Text)
+        intTotalInvCount += 1
+
+    End Sub
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        openFile()
+
+    End Sub
+
+    Private Sub mnuOpen_Click(sender As Object, e As EventArgs) Handles mnuOpen.Click
+        openFile()
+
+    End Sub
+
+    Private Sub frmListView_Load(sender As Object, e As EventArgs) Handles Me.Load
+        arrCategories = New ArrayList
+        stats = New frmStats
+    End Sub
+
+    Private Sub mnuStats_Click(sender As Object, e As EventArgs) Handles mnuStats.Click
+        stats.lstStats.Items.Clear()
+        With stats.lstStats
+            .Items.Add("Total Inventory Value = " & FormatCurrency(dblTotalInvValue))
+            .Items.Add("Total Inventory Count = " & CStr(intTotalInvCount))
+            For Each aCat As CCategory In arrCategories
+                .Items.Add(aCat.CatName & ":")
+                .Items.Add("  Value = " & FormatCurrency(aCat.TotalValue))
+                .Items.Add("  Count = " & CStr(aCat.TotalCount))
+
+            Next
+        End With
+        stats.ShowDialog()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lvwInventory.Items.Clear()
+        dblTotalInvValue = 0
+        intTotalInvCount = 0
+        arrCategories = New ArrayList
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        SaveFile()
+
+    End Sub
+
+    Private Sub mnuSave_Click(sender As Object, e As EventArgs) Handles mnuSave.Click
+        SaveFile()
+    End Sub
+End Class
